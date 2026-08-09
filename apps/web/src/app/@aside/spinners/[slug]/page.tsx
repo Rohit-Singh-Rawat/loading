dcb0dba8bb3bb2896d1a18dbd38b297b52e51926
Jakub Krehel <87530089+jakubkrehel@users.noreply.github.com>
2026-08-09 17:45:00 +0200

    fix: remove unnecessary padding from spinner aside component for improved layout

--- apps/web/src/app/@aside/spinners/[slug]/page.tsx
+++ apps/web/src/app/@aside/spinners/[slug]/page.tsx
@@ -42,7 +42,7 @@ export default async function SpinnerAside({
 
   return (
     <aside className="hidden w-[240px] shrink-0 py-[100px] xl:block">
-      <div className="sticky top-[100px] flex flex-col gap-4 pt-[144px]">
+      <div className="sticky top-[100px] flex flex-col gap-4">
         <CopyPageButton markdown={markdown} />
         <Toc items={TOC_ITEMS} />
       </div>
