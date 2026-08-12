099de53023c212d70aa6200a183278541e46bbde
Jakub Krehel <87530089+jakubkrehel@users.noreply.github.com>
2026-08-12 19:16:33 +0200

    fix: update OG_IMAGE URL to point to the correct image for improved asset management

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -8,6 +8,6 @@ export const TWITTER_HANDLE = "@jakubkrehel";
 
 export const OG_IMAGE = {
   height: 1008,
-  url: "https://ru2qm1zsj1gavqlm.public.blob.vercel-storage.com/og-image.png",
+  url: "https://ru2qm1zsj1gavqlm.public.blob.vercel-storage.com/og.png",
   width: 1920,
 } as const;
